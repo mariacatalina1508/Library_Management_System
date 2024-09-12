af184f19e09de61bf0d318f4d695a6d170567a55
Catalina <maria.catalina15082004@gmail.com>
2024-09-12 23:31:43 +0300

    Testing

---
+++ src/test/java/com/itschool/library/CalculatorTest.java
@@ -0,0 +1,47 @@
+package com.itschool.library;
+
+import com.itschool.library.utils.Calculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class CalculatorTest {
+
+    private static Calculator calculator;
+
+    @BeforeAll
+    static void setup() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testAddition_withValidInput_ShouldPass() {
+        //given
+        int firstNumber = 5;
+        int secondNumber = 10;
+        int expectResult = firstNumber + secondNumber;
+
+        //when
+        int actualResult = calculator.add(firstNumber, secondNumber);
+
+        //then
+        assertEquals(expectResult, actualResult, "The addition result should be equal to the expected result");
+    }
+
+    @Test
+    void testAddition_WithResultExceedsMaximumIntegerValue_ShouldFail() {
+        //given
+        int a = Integer.MAX_VALUE;
+        int b = 1;
+        long expectedResult = (long) a + b;
+
+        //when
+        int actualResult = calculator.add(a, b);
+
+        //then
+        assertNotEquals(expectedResult, actualResult);
+    }
+}
